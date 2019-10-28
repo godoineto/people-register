4354f02deb2ab6cd56ab17e7992a3278b473752c
Godoi <netogodoi.dev@gmail.com>
2019-10-28 13:03:26 -0300

    Adding load all people to person V1

--- src/main/java/com/people/register/api/person/v1/PersonV1Controller.java
+++ src/main/java/com/people/register/api/person/v1/PersonV1Controller.java
@@ -1,20 +1,20 @@
 package com.people.register.api.person.v1;
 
-import com.people.register.api.person.model.Gender;
 import com.people.register.api.person.model.Person;
 import com.people.register.api.person.v1.dto.NewPersonV1DTO;
 import com.people.register.api.person.v1.dto.PersonV1DTO;
 import com.people.register.api.person.worker.PersonCreator;
+import com.people.register.api.person.worker.PersonLoader;
 import com.people.register.config.GeneralController;
 import io.swagger.annotations.Api;
-import lombok.val;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Arrays;
-import java.util.Date;
+import javax.websocket.server.PathParam;
+import java.lang.reflect.Type;
 import java.util.List;
 
 @RestController
@@ -28,6 +28,9 @@ public class PersonV1Controller implements GeneralController {
     @Autowired
     private PersonCreator creator;
 
+    @Autowired
+    private PersonLoader loader;
+
     @PostMapping
     public PersonV1DTO add(@RequestBody @Valid NewPersonV1DTO newPerson) {
         return mapper.map(
@@ -36,32 +39,14 @@ public class PersonV1Controller implements GeneralController {
                 PersonV1DTO.class);
     }
 
+    @GetMapping("{id}")
+    public PersonV1DTO load(@PathParam("id") String id) {
+        return mapper.map(loader.list(), PersonV1DTO.class);
+    }
+
     @GetMapping
-    public List<NewPersonV1DTO> list() {
-        val karol = new NewPersonV1DTO();
-        karol.setName("Karol G Siegel");
-        karol.setGender(Gender.FEMALE);
-        karol.setEmail("karolgs@hotmail.com");
-        karol.setNationality("Brasileira");
-        karol.setPlaceOfBirth("Palhoça");
-        karol.setCpf("000.000.000-00");
-        karol.setBirth(new Date());
-        val neto = new NewPersonV1DTO();
-        neto.setName("Neto Godoi");
-        neto.setGender(Gender.MALE);
-        neto.setEmail("netogodoi.dev@gmail.com");
-        neto.setNationality("Brasileiro");
-        neto.setPlaceOfBirth("Lages");
-        neto.setCpf("089.457.839-11");
-        neto.setBirth(new Date());
-        val lia = new NewPersonV1DTO();
-        lia.setName("Lia Siegel Godoi");
-        lia.setGender(Gender.FEMALE);
-        lia.setEmail("chowchowlia@gmail.com");
-        lia.setNationality("Brasileira");
-        lia.setPlaceOfBirth("Palhoça");
-        lia.setCpf("000.000.000-00");
-        lia.setBirth(new Date());
-        return Arrays.asList(karol, neto, lia);
+    public List<PersonV1DTO> list() {
+        Type listType = new TypeToken<List<PersonV1DTO>>(){}.getType();
+        return mapper.map(loader.list(), listType);
     }
 }
